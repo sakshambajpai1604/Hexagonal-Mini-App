85a8b9567f832bb3494b009c6c6585baa1535085
sakshambajpai1604 <sakshambajpai.1604@gmail.com>
2026-06-20 14:37:10 +0530

    feat: wire ports and adapters through factory

--- src/main/java/app/Main.java
+++ src/main/java/app/Main.java
@@ -1,17 +1,28 @@
 package app;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+import domain.OrderService;
+import domain.PaymentPort;
+import domain.NotificationPort;
+import factory.AdapterFactory;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+public class Main {
+
+    public static void main(String[] args) {
+
+        String paymentProvider = "stripe";
+        String notificationType = "email";
+
+        PaymentPort payment =
+                AdapterFactory.createPaymentAdapter(
+                        paymentProvider);
+
+        NotificationPort notification =
+                AdapterFactory.createNotificationAdapter(
+                        notificationType);
+
+        OrderService orderService =
+                new OrderService(payment, notification);
+
+        orderService.placeOrder(1500);
     }
 }
